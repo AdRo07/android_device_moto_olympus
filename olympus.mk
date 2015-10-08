95f3d97fa3e28d4a85a8cdb946fea654f7836fa0
Hangbokie <sungkisa@naver.com>
2015-10-08 15:45:07 +0900

    Fix build
    PROUDCT_COPY_FILE error

--- olympus.mk
+++ olympus.mk
@@ -2,28 +2,32 @@
 #TODO After Boot, Copy other files
 BAD := device/moto/olympus
 PRODUCT_COPY_FILES := \
-    $(BAD)/init.olympus.rc:root/init.olympus.rc \
-    $(BAD)/init.olympus.usb.rc:root/init.olympus.usb.rc \
-    $(BAD)/ueventd.olympus.rc:root/ueventd.olympus.rc \
-    $(BAD)/kernel:root/kernel \
+    ${BAD}/init.olympus.rc:root/init.olympus.rc \
+    ${BAD}/init.olympus.usb.rc:root/init.olympus.usb.rc \
+    ${BAD}/ueventd.olympus.rc:root/ueventd.olympus.rc \
+	system/core/rootdir/init.trace.rc:root/init.trace.rc \
+	${BAD}/fstab.olympus:root/fstab.olympus \
+	device/moto/olympus/scripts/pds_perm_fix.sh:system/bin/pds_perm_fix.sh \
+	device/moto/olympus/scripts/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
+    ${BAD}/kernel:root/kernel 
 
 #Keyboard and Keylayout for olympus
 PRODUCT_COPY_FILES += \
-    $(BAD)/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    $(BAD)/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl:system/usr/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl \
-    $(BAD)/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
-    $(BAD)/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
-    $(BAD)/keylayout/qtouch-obp-ts.kl:system/usr/keylayout/qtouch-obp-ts.kl \
-    $(BAD)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    $(BAD)/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl \
-    $(BAD)/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
+    ${BAD}/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    ${BAD}/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl:system/usr/keylayout/Motorola_Mobility_Motorola_HD_Dock.kl \
+    ${BAD}/keylayout/cpcap-key.kl:system/usr/keylayout/cpcap-key.kl \
+    ${BAD}/keylayout/evfwd.kl:system/usr/keylayout/evfwd.kl \
+    ${BAD}/keylayout/qtouch-obp-ts.kl:system/usr/keylayout/qtouch-obp-ts.kl \
+    ${BAD}/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    ${BAD}/keylayout/tegra-kbc.kl:system/usr/keylayout/tegra-kbc.kl \
+    ${BAD}/keylayout/usb_keyboard_102_en_us.kl:system/usr/keylayout/usb_keyboard_102_en_us.kl \
 
 
 PRODUCT_COPY_FILES += \
-    $(BAD)/keychars/tegra-kbc.kcm.bin:system/usr/keychars/tegra-kbc.kcm.bin \
-    $(BAD)/keychars/evfwd.kcm.bin:system/usr/keychars/evfwd.kcm.bin \
-    $(BAD)/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/Motorola_Bluetooth_Wireless_Keyboard.kcm.bin \
-    $(BAD)/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/usb_keyboard_102_en_us.kcm.bin \
+    ${BAD}/keychars/tegra-kbc.kcm.bin:system/usr/keychars/tegra-kbc.kcm.bin \
+    ${BAD}/keychars/evfwd.kcm.bin:system/usr/keychars/evfwd.kcm.bin \
+    ${BAD}/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/Motorola_Bluetooth_Wireless_Keyboard.kcm.bin \
+    ${BAD}/keychars/usb_keyboard_102_en_us.kcm.bin:system/usr/keychars/usb_keyboard_102_en_us.kcm.bin \
 
 # Permission files
 PRODUCT_COPY_FILES += \
