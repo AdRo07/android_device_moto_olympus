357712584dfb586f5bacd6ecfea2a6033926eb6b
HeungJu <sungkisa@naver.com>
2015-11-27 16:03:27 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,12 +12,7 @@ TARGET_KERNEL_CONFIG := tegra_olympus_defconfig
 BOARD_KERNEL_BASE := 0x00000000
 #0x00000800 = 2048
 BOARD_KERNEL_PAGESIZE :=  2048
-#BOARD_KERNEL_TAGS_OFFSET := We don't need it!
-#BOARD_RAMDISK_OFFSET     := We don't have this offset :)
 
-#BOARD_KERNEL_CMDLINE := Shamu! Why you are using long and dirty CMDLINE? olympus doesn't have it
-
-#BOARD_MKBOOTIMG_ARGS := This options too!
 
 BOARD_EGL_CFG := device/moto/olympus/egl.cfg
 
@@ -26,14 +21,14 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := tegra
 TARGET_BOOTLOADER_BOARD_NAME := olympus
 
-#Important! You must add this options
+
 USE_OPENGL_RENDERER := true
 
 TARGET_BOARD_INFO_FILE := device/moto/olympus/board-info.txt
 
 
 
-## KB options! No MB or GB  ##
+
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8355840
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 81922
